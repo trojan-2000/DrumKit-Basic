b20f96c7217395affd2442e709aa5194d56ffa47
trojan-2000 <68050159+trojan-2000@users.noreply.github.com>
2020-10-22 23:02:50 +0530

    Playing sound Bug fixed

--- js/myscript.js
+++ js/myscript.js
@@ -1,81 +1,135 @@
-
 document.addEventListener("keypress",makeSound);
+var crashcymbal=new Audio("sounds/crashcymbal.mp3");
+var floortom=new Audio("sounds/floortom.mp3");
+var tomtom=new Audio("sounds/tomtom.wav");
+var midtom=new Audio("sounds/midtom.mp3");
+var snare=new Audio("sounds/snare.wav");
+var hihat=new Audio("sounds/hihat.mp3");
+var bassdrum=new Audio("sounds/bassdrum.mp3");
 
 var buttonList=document.querySelectorAll("button");
 for(var item of buttonList){
-	item.addEventListener("click",function(){
-		switch(this.innerHTML){
-
-		case "W":
-			var audio=new Audio("sounds/crashcymbal.mp3");
-			audio.play();
-			break;
-		case "A":
-			var audio=new Audio("sounds/floortom.mp3");
-			audio.play();
-			break;
-		case "S":
-			var audio=new Audio("sounds/tomtom.wav");
-			audio.play();
-			break;
-		case "J":
-			var audio=new Audio("sounds/midtom.mp3");
-			audio.play();
-			break;
-		case "K":
-			var audio=new Audio("sounds/snare.wav");
-			audio.play();
-			break;
-		case "L":
-			var audio=new Audio("sounds/hihat.mp3");
-			audio.play();
-			break;
-		case "D":
-			var audio=new Audio("sounds/bassdrum.mp3");
-			audio.play();
-			break;
-		default:
-			alert("Invalid");
-
-	}
-	});
+item.addEventListener("click",function(){
+switch(this.innerHTML){
+case "W":
+if(!crashcymbal.paused){
+	crashcymbal.pause();
+	crashcymbal.currentTime=0;
 }
 
+crashcymbal.play();
+break;
+case "A":
+if(!floortom.paused){
+	floortom.pause();
+	floortom.currentTime=0;
+}
+
+floortom.play();
+break;
+case "S":
+if(!tomtom.paused){
+	tomtom.pause();
+	tomtom.currentTime=0;
+}
+
+tomtom.play();
+break;
+case "J":
+if(!midtom.paused){
+	midtom.pause();
+	midtom.currentTime=0;
+}
+
+	midtom.play();
+break;
+case "K":
+if(!snare.paused){
+	snare.pause();
+	snare.currentTime=0;
+}
+
+	snare.play();
+break;
+case "L":
+if(!hihat.paused){
+	hihat.pause();
+	hihat.currentTime=0;
+}
+
+hihat.play();
+break;
+case "D":
+if(!bassdrum.paused){
+	bassdrum.pause();
+	bassdrum.currentTime=0;
+}
+
+bassdrum.play();
+break;
+default:
+// alert("Invalid");
+}
+});
+}
 function makeSound(event){
+var x=event.key;
+switch(x){
+case "w":
+if(!crashcymbal.paused){
+	crashcymbal.pause();
+	crashcymbal.currentTime=0;
+}
 
-	var x=event.key;
-	switch(x){
+crashcymbal.play();
+break;
+case "a":
+if(!floortom.paused){
+	floortom.pause();
+	floortom.currentTime=0;
+}
 
-		case "w":
-			var audio=new Audio("sounds/crashcymbal.mp3");
-			audio.play();
-			break;
-		case "a":
-			var audio=new Audio("sounds/floortom.mp3");
-			audio.play();
-			break;
-		case "s":
-			var audio=new Audio("sounds/tomtom.wav");
-			audio.play();
-			break;
-		case "j":
-			var audio=new Audio("sounds/midtom.mp3");
-			audio.play();
-			break;
-		case "k":
-			var audio=new Audio("sounds/snare.wav");
-			audio.play();
-			break;
-		case "l":
-			var audio=new Audio("sounds/hihat.mp3");
-			audio.play();
-			break;
-		case "d":
-			var audio=new Audio("sounds/bassdrum.mp3");
-			audio.play();
-			break;
-		default:
-			alert("Invalid");
+floortom.play();
+break;
+case "s":
+if(!tomtom.paused){
+	tomtom.pause();
+	tomtom.currentTime=0;
+}
 
-	}
+tomtom.play();
+break;
+case "j":
+if(!midtom.paused){
+	midtom.pause();
+	midtom.currentTime=0;
+}
+	midtom.play();
+break;
+case "k":
+if(!snare.paused){
+	snare.pause();
+	snare.currentTime=0;
+}
+	snare.play();
+break;
+case "l":
+if(!hihat.paused){
+	hihat.pause();
+	hihat.currentTime=0;
+}
+
+hihat.play();
+break;
+case "d":
+if(!bassdrum.paused){
+	bassdrum.pause();
+	bassdrum.currentTime=0;
+}
+
+bassdrum.play();
+break;
+default:
+// alert("Invalid");
+}
 }
